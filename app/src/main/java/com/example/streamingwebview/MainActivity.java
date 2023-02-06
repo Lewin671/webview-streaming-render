538f6f768b310448358d219385ffe10db82833c7
qingyingliu <2596736318@qq.com>
2023-02-06 23:43:27 +0800

    feat: commit streaming inputStream codes

---
+++ app/src/main/java/com/example/streamingwebview/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.streamingwebview;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView.setWebContentsDebuggingEnabled(true);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Nullable
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                if (request != null) {
+                    Uri uri = request.getUrl();
+                    if (uri != null) {
+                        String url = uri.toString();
+                        Log.e("MYtAG", url);
+                        if ("https://www.baidu.com/".equals(url)) {
+                            Log.e("myTag", "url matched");
+
+                            PipedInputStream pipedInputStream = new PipedInputStream() {
+                                @Override
+                                public byte[] readAllBytes() throws IOException {
+                                    Log.e("myTag", "readAllBytes()");
+                                    return super.readAllBytes();
+                                }
+
+                                @Override
+                                public int read(byte[] b) throws IOException {
+                                    Log.e("myTag", "read(byte[] b)");
+                                    return super.read(b);
+                                }
+
+                                @Override
+                                public synchronized int read() throws IOException {
+                                    Log.e("myTag", "read");
+                                    return super.read();
+                                }
+
+                                @Override
+                                public synchronized int read(byte[] b, int off, int len) throws IOException {
+                                    Log.e("myTag", "read(byte[] b, int off, int len)");
+                                    return super.read(b, off, len);
+                                }
+
+                            };
+                            PipedOutputStream pipedOutputStream = new PipedOutputStream();
+                            try {
+                                pipedOutputStream.connect(pipedInputStream);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            new Thread() {
+                                @Override
+                                public void run() {
+                                    super.run();
+                                    int cnt = 0;
+                                    String beforeBody = "<!DOCTYPE html>\n" +
+                                            "<html lang=\"en\">\n" +
+                                            "<head>\n" +
+                                            "    <meta charset=\"UTF-8\">\n" +
+                                            "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                                            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                                            "    <title>Document</title>\n" +
+                                            "</head>\n" +
+                                            "<script>\n" +
+                                            "    var cnt = 0;\n" +
+                                            "    setInterval(() => {\n" +
+                                            "        console.log(\"current: \" + cnt);\n" +
+                                            "        cnt = cnt + 1;\n" +
+                                            "    }, 1000);\n" +
+                                            "</script>\n" +
+                                            "<body>";
+                                    String afterBody = "</body>\n" +
+                                            "</html>";
+                                    try {
+                                        pipedOutputStream.write(beforeBody.getBytes(StandardCharsets.UTF_8));
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+//                                    try {
+//                                        pipedOutputStream.close();
+//                                    } catch (IOException e) {
+//                                        e.printStackTrace();
+//                                    }
+                                    while (cnt < 5) {
+                                        Log.e("myTag", "cnt = " + cnt);
+                                        try {
+                                            Thread.sleep(1000);
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                        try {
+                                            pipedOutputStream.write(("<p> current： " + cnt + "</p>").getBytes());
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                        cnt++;
+                                        try {
+                                            pipedOutputStream.flush();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+
+                                    try {
+                                        pipedOutputStream.write(afterBody.getBytes(StandardCharsets.UTF_8));
+                                        pipedOutputStream.close();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }.run();
+
+                            return new WebResourceResponse("text/html", StandardCharsets.UTF_8.name(), pipedInputStream);
+                        }
+                    }
+                }
+                return super.shouldInterceptRequest(view, request);
+            }
+        });
+        webView.loadUrl("https://www.baidu.com/");
+
+        setContentView(webView);
+    }
+}
